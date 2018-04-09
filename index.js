6e208a86b3177e19ee2acd51e26e65fb6dc6f06d
tj11 <tedwardj11@gmail.com>
2018-04-09 17:40:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length-1; i++) {
+  for (var i = 0; i <= cart.length-1; i++) {
     if (cart[i].itemName == item) {
       cart.splice(i,1);
       return cart;
