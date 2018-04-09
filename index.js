24056893419aea849554669ee91099385667755f
tj11 <tedwardj11@gmail.com>
2018-04-09 13:24:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- objnewItem = { itemName:item, itemPrice: Math.floor(Math.random()*100+1) }
+var newItem = { itemName:item, itemPrice: Math.floor(Math.random()*100+1) }
  
  cart.push(newItem)
  
