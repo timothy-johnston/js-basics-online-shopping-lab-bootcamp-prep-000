5ff1d1d39e872037478a3b146449f017f911149f
tj11 <tedwardj11@gmail.com>
2018-04-09 18:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     var totalCost=total();
+    
     return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
