5a5708c202cc74bbda77425fe0a34a577f29974d
tj11 <tedwardj11@gmail.com>
2018-04-09 13:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function viewCart() {
   if (cart.length == 0) {
     return 'Your shopping cart is empty.'
   } else if (cart.length == 1) {
-    return `In your cart, you have ${cart.[1].itemName} at $${cart[1].itemprice
+    return `In your cart, you have ${cart[1].itemName} at $${cart[1].itemprice}.`
+  } else if (cart.length == 2) {
+    return ''
   }
     
   }
