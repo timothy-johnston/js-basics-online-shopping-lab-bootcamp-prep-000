731eea78c21893b7dc3c190e381f4ac4c4085678
tj11 <tedwardj11@gmail.com>
2018-04-09 17:59:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function placeOrder(cardNumber) {
   if (typeof cardNumber == 'undefined') {
     return "Sorry, we don't have a credit card on file for you."
   } else {
+    cart=cart.splice(0,cart.length)
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
