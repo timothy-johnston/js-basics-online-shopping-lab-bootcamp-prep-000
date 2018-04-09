5aa93553645c03ea6d1a5284913d52c2240ced1f
tj11 <tedwardj11@gmail.com>
2018-04-09 14:21:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function viewCart() {
   if (cart.length === 0) {
     return 'Your shopping cart is empty.'
   } else if (cart.length == 1) {
-    return `In your cart, you have ${cart[1].itemName} at $${cart[1].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else if (cart.length == 2) {
-    return `In your cart, you have ${cart[1].itemName} at $${cart[1].itemprice} and ${cart[2].itemName} at $${cart[2].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemprice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else if (cart.length > 2) {
     var sentence = `In your cart, you have `
     for (var i=0; i<cart.length-1; i++) {
