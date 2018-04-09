50383c25312a125be637faa9affc052a20355cc0
tj11 <tedwardj11@gmail.com>
2018-04-09 13:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
   if (cart.length == 0) {
     return 'Your shopping cart is empty.'
   } else if (cart.length == 1) {
-    return `In your cart, you have $[c]
+    return `In your cart, you have ${cart.[1].itemName} at $${cart[1].itemprice
   }
     
   }
